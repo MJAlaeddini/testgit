bc5b41022fe472f5cdd97b59e65cb73b5b0392a3
MohammadJavad Alaeddini <javad1379.a@gmail.com>
2020-07-02 11:52:49 +0430

    add

--- .idea/src/main.java
+++ .idea/src/main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("dsv");
         System.out.println("sc");
+        System.out.println("salam");
     }
 }
